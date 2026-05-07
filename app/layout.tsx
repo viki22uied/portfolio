3f5e0c099b7805aebd8388b888ebbd5a56a3409b
viki22uied <vigneshkumaru24@gmail.com>
2026-05-07 17:23:03 +0530

    Up to date

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata: Metadata = {
     icon: [
       { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
       { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
     ],
     apple: '/apple-icon.png',
   },
